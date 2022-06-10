ff3a99a569bfb9ed7deb6b4c6efe11b551ec8060
TinchoLC <104269861+TinchoLC@users.noreply.github.com>
2022-06-09 23:22:54 -0300

    Falta separar al .h y bueno el resto jaja

---
+++ archivos.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct _SNodo {
+	char *provincia;
+	int v_habitadas;
+	int v_deshabitadas;
+	int v_colectivas;
+	struct _SNodo *sig;
+} SNodo;
+
+typedef SList *Snodo;
+
+
+SNodo* slist_agregar_final(SNodo* lista, char const *provincia, int v_habitadas, int v_deshabitadas, int v_colectivas) {
+	SNodo *nuevo_nodo = malloc(sizeof(SNodo));
+
+  largo_nombre_provincia = strlen(provincia);
+  nuevo_nodo->provincia = malloc(sizeof(char) * (largo_nombre_provincia + 1));
+  provincia;
+  nuevo_nodo->v_habitadas = v_habitadas;
+  nuevo_nodo->v_deshabitadas = v_deshabitadas;
+  nuevo_nodo->v_colectivas = v_colectivas;
+  nuevo_nodo->sig = NULL;
+
+  if (lista == NULL)
+    return nuevo_nodo;
+
+  SList nodo_temp = lista;
+  for (; nodo_temp->sig != NULL; nodo_temp = nodo_temp->sig);
+
+  nodo_temp->sig = nuevo_nodo;
+  return lista;
+}
+
+
+SNodo* csv_a_archivo(char const* viviendas_provincia){
+	SNodo* lista = NULL;
+	FILE* provincia_info = abrir_archivo(viviendas_provincia,"r");
+
+	for(int i = 0; EOF != fscanf(provincia_info, "%s,%d,%d,%d\n", provincia, v_habitadas, v_deshabitadas, v_colectivas); i++)
+		lista=slist_agregar_final(lista, provincia, v_habitadas, v_deshabitadas, v_colectivas);
+
+	fclose(provincia_info);
+}
