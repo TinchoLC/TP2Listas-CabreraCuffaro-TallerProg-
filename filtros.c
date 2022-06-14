ebd39de2e4d826b68a5c87cb4e97322dacb66f90
TinchoLC <104269861+TinchoLC@users.noreply.github.com>
2022-06-14 20:32:45 -0300

    Update filtros.c

--- filtros.c
+++ filtros.c
@@ -3,8 +3,8 @@
 SList filter_listas(SList lista, Predicado p){
 	SList lista_nueva = NULL;
 
-	for(; lista != NULL; lista = lista->sig){
-		if(p(lista))
+	for (; lista != NULL; lista = lista->sig) {
+		if (p(lista))
 			lista_nueva = slist_agregar_final(lista_nueva, lista->provincia, lista->v_habitadas, lista->v_deshabitadas, lista->v_colectivas);
 	}
 	return lista_nueva;
@@ -16,9 +16,9 @@ int mayor_mil_v_deshabitadas(SList lista){
 
 
 int filter_fold_listas(SList lista, Predicado p, Operador o){
-	SList listita = filter_listas(lista, p);
-	int operacion = o(listita);
-	slist_destruir(listita);
+	SList lista_auxiliar = filter_listas(lista, p);
+	int operacion = o(lista_auxiliar);
+	slist_destruir(lista_auxiliar);
 	return operacion;
 }
 
@@ -28,7 +28,7 @@ int mayor_mil_v_colectivas(SList lista){
 
 int suma_viviendas_totales(SList lista){
 	int suma = 0;
-	for(SList nodo = lista; nodo != NULL;  nodo = nodo->sig){
+	for (SList nodo = lista; nodo != NULL; nodo = nodo->sig) {
 		suma += nodo->v_habitadas;
 		suma += nodo->v_deshabitadas;
 	}
